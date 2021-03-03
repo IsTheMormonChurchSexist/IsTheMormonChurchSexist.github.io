da16fa51bbb405087e467a496b800b73f45ca504
ChristineEliGroux <65317685+ChristineEliGroux@users.noreply.github.com>
2021-03-03 09:24:23 -0500

    Links to quotes

--- RQG/functions.js
+++ RQG/functions.js
@@ -11,17 +11,14 @@ fetch(QUOTES_JSON_FILE_PATH)
        newQuote()  
 }); 
 
-function linkToPage(linkSource) {
-
-}
-
 const newQuote = () => {
     var randomIndex = Math.floor(Math.random() * (data.length));
+    var quote = data[randomIndex];
 
-    document.getElementById('quoteDisplay').innerHTML = data[randomIndex].text; 
-    document.getElementById('authorDisplay').innerHTML = data[randomIndex].author; 
-    document.getElementById('dateDisplay').innerHTML = data[randomIndex].year; 
+    document.getElementById('quoteDisplay').innerHTML = quote.text; 
+    document.getElementById('authorDisplay').innerHTML = quote.author; 
+    document.getElementById('dateDisplay').innerHTML = quote.year; 
 
-    document.getElementById("buttonSource").setAttribute("onClick", ); 
+    document.getElementById('sourceDisplay').innerHTML = quote.source;
+    document.getElementById('sourceDisplay').setAttribute("href", quote.weblink);
 }
-"window.open('https://google.com')"
