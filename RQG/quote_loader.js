0502795911c8897043d9aab371cb35cbc59023ec
ChrstnGrx <christinegroux1@gmail.com>
2021-03-19 22:41:00 -0400

    Visual test failed

--- RQG/quote_loader.js
+++ RQG/quote_loader.js
@@ -3,7 +3,11 @@ const QUOTES_JSON_FILE_PATH = 'quotes.json';
 $(document).ready(() => {
     let quotesListAnchor = $('quotes-list-anchor');
 
-    fetchFromJson(QUOTES_JSON_FILE_PATH, quotesData => {
+    const nb = `4`;
+    let element = buildElement(nb);
+    $(quotesListAnchor).append(element);
+
+    /**fetchFromJson(QUOTES_JSON_FILE_PATH, quotesData => {
         quotesData.forEach((quote, i) => {
             
             const ID = `quote-${i}`;
@@ -12,7 +16,7 @@ $(document).ready(() => {
             $(quotesListAnchor).append(quoteListElement);
 
         });
-    });
+    });**/
 });
 
 function fetchFromJson(jsonFilePath, callback) {
@@ -23,6 +27,14 @@ function buildQuoteListElement(quoteData, identifier) {
     return `
             <div>
                 test
+            </div>
+    `;
+}
+
+function buildElement(number) {
+    return `
+            <div>
+                test ${number}
             </div>
     `;
 }
